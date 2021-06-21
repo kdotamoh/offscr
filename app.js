bd90c5c1629a0a2fb9cdc742199b2e74577e18bf
kdotamoh <kwadwo.amoh@gmail.com>
2021-06-21 12:24:50 +0000

    build: :rocket: change default port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const port = 1376;
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
@@ -11,6 +13,6 @@ app.get("/", (req, res) => {
   res.render("pages/home");
 });
 
-app.listen("3000", () => {
-  console.log("listening on http://localhost:3000");
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
 });
